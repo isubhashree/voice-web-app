1e20af129f4810c1229237909eecb63e46f5c8f5
isubhashree <iam.subhashreepatra@gmail.com>
2016-02-29 03:24:02 +0530

    p to P

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var authToken = 'f91dc02a675a9f0468cfde682f53d2e3';
 var client = require('twilio')(accountSid, authToken);
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/Public'));
 
 app.get('/', function (req, res) {
    res.sendFile( __dirname + "/" + "WebVoiceApp.html" );
